aac7de6a956a07ba669b422f617273faf0461b29
Likith <likithsatya192@gmail.com>
2025-05-18 15:10:29 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from sentimentAnalysis.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from sentimentAnalysis.logging import logger
+from sentimentAnalysis.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from sentimentAnalysis.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -8,6 +9,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
     logger.exception(e)
